ab5830e27f507d29501969904c59421582b51f72
Craig Storer <craigstorer@gmail.com>
2020-03-05 20:03:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
 
-def countdown
+
+def countdown(num)
+ while num > 0 
+  num -= 1
+  # sleep(1)
+  puts "#{num} SECOND(S)!"
+if num == 0
+ return "HAPPY NEW YEAR!"
 end
+end
+end
+
+def countdown_with_sleep(num)
+   while num > 0 
+  num -= 1
+  sleep(1)
+  puts "#{num} SECOND(S)!"
+if num == 0
+ return "HAPPY NEW YEAR!"
+end
+end
+end
+
+
